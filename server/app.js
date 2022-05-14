3e6971fff8afec6e1c508b5eba7cada8d63952ec
asawadichai <sawadichai.andrew@gmail.com>
2022-05-14 10:48:17 -0700

    Complete Parsing Cookie Complete Createsession Middleware

--- server/app.js
+++ server/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const utils = require('./lib/hashUtils');
 const partials = require('express-partials');
+const bodyParser = require('body-parser');
 const Auth = require('./middleware/auth');
-const cookieParser = require('./middleware/cookieParser');
+const cookie = require('./middleware/cookieParser');
 const models = require('./models');
 
 const app = express();
@@ -11,42 +12,49 @@ const app = express();
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 app.use(partials());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cookie);
+app.use(Auth.createSession);
 
-// app.use(Auth.createSession);
-// app.use(cookieParser);
+const verifySession = function(req, res, cb) {
+  if (!req.session.userId) {
+    res.redirect('/login');
+  } else {
+    cb();
+  }
+};
 
 app.get('/',
   (req, res) => {
-    res.render('index');
+    verifySession.call(this, req, res, () => res.render('index'));
   });
 
 app.get('/create',
-  ( req, res) => {
-    res.render('index');
+  (req, res) => {
+    verifySession.call(this, req, res, () => res.render('index'));
   });
 
 app.get('/links',
   (req, res, next) => {
-    models.Links.getAll()
-      .then(links => {
-        res.status(200).send(links);
-      })
-      .error(error => {
-        res.status(500).send(error);
-      });
+    verifySession.call(this, req, res, () => {
+      models.Links.getAll()
+        .then(links => {
+          res.status(200).send(links);
+        })
+        .error(error => {
+          res.status(500).send(error);
+        });
+    });
   });
 
 app.post('/links',
   (req, res, next) => {
     var url = req.body.url;
     if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
       return res.sendStatus(404);
     }
-
     return models.Links.get({ url })
       .then(link => {
         if (link) {
@@ -78,63 +86,57 @@ app.post('/links',
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
-app.get('/signup', (req, res) => {
-  res.render('signup');
-  res.cookieParser();
-  res.sendStatus(200);
-});
+
+app.get('/login',
+  (req, res) => {
+    res.render('login');
+  });
 
 app.post('/signup', (req, res) => {
   var username = req.body.username;
-  return models.Users.get({username})
-    .then((user) => {
-      if (user) {
-        res.redirect('/signup'); // /login if username exists?
+  models.Users.get({ username })
+    .then(data => {
+      if (data) {
+        res.redirect('/signup');
       } else {
-        var username = req.body.username;
-        var password = req.body.password;
-        models.Users.create({username, password});
+        models.Users.create(req.body)
+          .then((data) => {
+            return models.Sessions.update({ hash: req.session.hash }, { userId: data.insertId });
+          })
+          .then((data) => {
+            req.session.userId = data.insertId;
+            req.session.user = { username: req.body.username };
+            res.redirect('/');
+          });
       }
-    })
-    .then((result) => {
-      res.redirect('/');
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      throw err;
     });
 });
 
-app.get('/login', (req, res, next) => {
-  res.render('login');
-  res.cookieParser();
-  res.sendStatus(200);
-});
-
 app.post('/login', (req, res) => {
-  var attempted = req.body.password;
   var username = req.body.username;
-  return models.Users.get({username})
-    .then((user) => {
-      if (user) {
-        if (models.Users.compare(attempted, user.password, user.salt)) {
-          res.redirect('/');
+  var password = req.body.password;
+  models.Users.get({ username })
+    .then(data => {
+      if (data) {
+        if (models.Users.compare(password, data.password, data.salt)) {
+          models.Sessions.update({ hash: req.session.hash }, { userId: data.id })
+            .then((data) => {
+              req.session.userId = data.insertId;
+              req.session.user = { username };
+              res.redirect('/');
+            });
+
         } else {
           res.redirect('/login');
         }
       } else {
         res.redirect('/login');
       }
-    })
-    .catch((error) => {
-      console.log('this is the error message for login', error);
     });
 });
 
 
-
-
-/***********s*************************************************/
+/************************************************************/
 // Handle the code parameter route last - if all other routes fail
 // assume the route is a short code and try and handle it here.
 // If the short-code doesn't exist, send the user to '/'
